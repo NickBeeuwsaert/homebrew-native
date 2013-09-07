2a33e820bbaa70f419b2e72fd623fbb724063e9e
r4z0rw0lf <nick.beeuwsaert@gmail.com>
2013-09-07 14:58:14 -0500

    added gtk+3

---
+++ gtk+3-native.rb
@@ -0,0 +1,49 @@
+require 'formula'
+
+# Documentation: https://github.com/mxcl/homebrew/wiki/Formula-Cookbook
+#                /usr/local/Library/Contributions/example-formula.rb
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class Gtkx3Native < Formula
+  homepage ''
+  url 'http://ftp.gnome.org/pub/gnome/sources/gtk+/3.8/gtk+-3.8.2.tar.xz'
+  sha1 'c519b553b618588f288c70ea5dce1145588944eb'
+
+  # depends_on 'cmake' => :build
+  #depends_on :x11 # if your formula requires any X11/XQuartz components
+  depends_on 'pkg-config' => :build
+  depends_on 'xz' => :build
+  depends_on 'glib'
+  depends_on 'jpeg'
+  depends_on 'libtiff'
+  depends_on 'gdk-pixbuf'
+  depends_on 'pango-native'
+  depends_on 'cairo-native'
+  depends_on 'gtk+-native'
+  depends_on 'jasper' => :optional
+  depends_on 'atk'
+  depends_on 'at-spi2-atk'
+  
+  def install
+    # ENV.j1  # if your formula's build system can't parallelize
+
+    # Remove unrecognized options if warned by configure
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--enable-quartz-backend",
+                          "--enable-gtk2-dependency",
+                          "--prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
+    system "make", "install" # if this fails, try separate make/make install steps
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! It's enough to just replace
+    # "false" with the main program this formula installs, but it'd be nice if you
+    # were more thorough. Run the test with `brew test gtk+3-native`.
+    system "false"
+  end
+end
